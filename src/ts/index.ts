4e61053c879b28aef49dd76720033cecc27e3317
Gabriel <quontas@gmail.com>
2020-11-09 15:16:18 -0500

    First migration.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,39 +1,42 @@
+const REPLACEMENT_INTERVAL = 2500;
+
 interface TextWithEmphasis {
   text: [string] | [string, string];
   emphasized?: boolean;
 }
 
 const TEXT_POOL: TextWithEmphasis[] = [
-  { text: ["service design", "woo"] },
-  { text: ["business models", "wa"] },
-  { text: ["human-centric", "Wee"] },
-  { text: ["designers", "humans"] },
-  { text: ["apps", "schmapps"] },
-  { text: ["singapore", "sing some more"] },
+  {text: ['filer text', 'filled']},
+  {text: ['lorem ipsum', 'dolor sic']},
+  {text: ['ipswich', 'england']},
+  {text: ['port aransas', 'texas']},
+  {text: ['apps', 'schmapps']},
+  {text: ['vanilla', 'extract']},
 ];
 
 const elementToStorage = (element: Element): TextWithEmphasis => {
-  let textWithEmphasis: TextWithEmphasis = { text: [""] };
-  if (element.tagName === "SPAN") {
+  const textWithEmphasis: TextWithEmphasis = {text: ['']};
+  if (element.tagName === 'SPAN') {
     textWithEmphasis.text = [element.textContent!];
-  } else if (element.tagName === "DIV") {
-    const tag = element.getElementsByClassName("normal-text")[0].textContent!;
-    const quip = element.getElementsByClassName("quip")[0].textContent!;
+  } else if (element.tagName === 'DIV') {
+    const tag = element.getElementsByClassName('normal-text')[0].textContent!;
+    const quip = element.getElementsByClassName('quip')[0].textContent!;
     textWithEmphasis.text = [tag, quip];
   } else {
-    throw new Error("Only DIVs and SPANs can be converted to storage.");
+    throw new Error('Only DIVs and SPANs can be converted to storage.');
   }
-  if (element.classList.contains("emphasized")) {
+  if (element.classList.contains('emphasized')) {
     textWithEmphasis.emphasized = true;
   }
   return textWithEmphasis;
 };
 
-const randomIndex = (collection: any[] | HTMLCollectionOf<Element>) =>
-  Math.floor(Math.random() * collection.length);
+const randomIndex = (
+  collection: TextWithEmphasis[] | HTMLCollectionOf<Element>
+) => Math.floor(Math.random() * collection.length);
 
 setInterval(() => {
-  const currentlyDisplayedTags = document.getElementsByClassName("tag");
+  const currentlyDisplayedTags = document.getElementsByClassName('tag');
   const randomCurrentlyDisplayedIndex = randomIndex(currentlyDisplayedTags);
   const randomIndexOfTagPool = randomIndex(TEXT_POOL);
 
@@ -43,31 +46,31 @@ setInterval(() => {
   let newTag: Element;
   if (newValue.text.length === 1) {
     const [normalText] = newValue.text;
-    newTag = document.createElement("span");
+    newTag = document.createElement('span');
     const textNode = document.createTextNode(normalText);
     newTag.appendChild(textNode);
-    newTag.className = "tag";
+    newTag.className = 'tag';
   } else {
     const [normalText, quip] = newValue.text;
-    newTag = document.createElement("div");
+    newTag = document.createElement('div');
 
-    const normalTextSpan = document.createElement("span");
+    const normalTextSpan = document.createElement('span');
     const normalTextNode = document.createTextNode(normalText);
     normalTextSpan.appendChild(normalTextNode);
-    normalTextSpan.className = "normal-text";
+    normalTextSpan.className = 'normal-text';
     newTag.appendChild(normalTextSpan);
 
-    const quipSpan = document.createElement("span");
+    const quipSpan = document.createElement('span');
     const quipTextNode = document.createTextNode(quip);
     quipSpan.appendChild(quipTextNode);
-    quipSpan.className = "quip";
+    quipSpan.className = 'quip';
     newTag.appendChild(quipSpan);
 
-    newTag.className = "tag";
+    newTag.className = 'tag';
     console.log(newTag.innerHTML);
   }
   if (newValue.emphasized) {
-    newTag.classList.add("emphasized");
+    newTag.classList.add('emphasized');
   }
   tagToReplace.parentNode?.replaceChild(newTag, tagToReplace);
-}, 100000);
+}, REPLACEMENT_INTERVAL);
